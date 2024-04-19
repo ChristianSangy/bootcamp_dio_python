509be9afb12fcaa06b324dd5c01e4aab5672d814
Ygor Fernandes <ygorkarateca@gmail.com>
2024-04-18 22:51:38 -0300

    aceita meu pr ae

---
+++ desafios-dio/desafio_meses.py
@@ -0,0 +1,6 @@
+month = int(input())
+
+months_dict = ["January", "February", "March", "April", "May", "June",
+         "July", "August", "September", "October", "November", "December"]
+
+print(months_dict[month - 1])
