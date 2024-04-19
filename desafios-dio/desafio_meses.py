0b194c0e61f6316f79468bd5ee2a8a1ae03fec15
ChristianSangy <167333208+ChristianSangy@users.noreply.github.com>
2024-04-18 22:59:31 -0300

    Update desafio_meses.py

--- desafios-dio/desafio_meses.py
+++ desafios-dio/desafio_meses.py
@@ -1,6 +1,19 @@
 month = int(input())
 
-months_dict = ["January", "February", "March", "April", "May", "June",
-         "July", "August", "September", "October", "November", "December"]
+months_dict = {
+    1 : "January",
+    2 : "February",
+    3 : "March",
+    4 : "April",
+    5 : "May",
+    6 : "June",
+    7 : "July",
+    8 : "August",
+    9 : "September",
+    10 : "October",
+    11 : "November",
+    12 : "December",
+}
 
-print(months_dict[month - 1])
+if month in months_dict:
+    print(months_dict[month])  
